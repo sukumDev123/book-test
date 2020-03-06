9eae685dceb7a558e8ba01df0aba89d47be630f9
kungo000 <kungo2009@hotmail.com>
2020-03-06 11:14:59 +0700

    add attr

--- BookApp/forms.py
+++ BookApp/forms.py
@@ -1,12 +1,19 @@
 from django import forms
 from .models import BookModel
 
+
 class AddNewBookForm(forms.ModelForm):
     class Meta:
         model = BookModel
-        fields = ['name_book', 'name_author', 'detail']
+        fields = ["name_book", "name_author", "detail"]
         widgets = {
-            "name_book" : forms.TextInput(attrs={'class': "form-control" , "placeholder": "name book..."}),
-            "name_author" : forms.TextInput(attrs={'class': "form-control" , "placeholder" : "name author..."}),
-            "detail": forms.Textarea(attrs={'class': "form-control" , "placeholder" : "detail..."})
+            "name_book": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "name book..."}
+            ),
+            "name_author": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "name author..."}
+            ),
+            "detail": forms.Textarea(
+                attrs={"class": "form-control", "placeholder": "detail..."}
+            ),
         }
