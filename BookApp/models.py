a94a04ac6e860659edf757bd8291769bb275931e
kungo000 <kungo2009@hotmail.com>
2020-03-06 16:08:30 +0700

    update model

--- BookApp/models.py
+++ BookApp/models.py
@@ -5,3 +5,4 @@ class BookModel(models.Model):
     name_book = models.CharField(max_length=255, blank=True)
     name_author = models.CharField(max_length=255, blank=True)
     detail = models.TextField()
+
